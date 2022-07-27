8935feb5cd56d40a1cba252736747d8fe37a5d0c
Daiane Melo <dai.melo.ca@gmail.com>
2022-07-27 11:56:19 -0600

    Java Básico

---
+++ src/main/java/br/com/dai/javabasico/JavaBasicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dai.javabasico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBasicoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/dai/javabasico/MultiplosDeTresAteCem.java
@@ -0,0 +1,8 @@
+package br.com.dai.javabasico;
+class MultiplosDeTresAteCem {
+	public static void main(String[] args) {
+		for (int i = 3; i < 100; i += 3) {
+			System.out.println(i);
+		}
+	}
+}
